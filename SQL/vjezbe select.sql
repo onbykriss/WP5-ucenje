00a8d04b2435066f9876b01322a7dc02b5880024
Kristina Andric <fashionbykriss@gmail.com>
2024-06-22 16:09:39 +0200

    vjezba select

--- SQL/vjezbe select.sql
+++ SQL/vjezbe select.sql
@@ -7,9 +7,83 @@ select * from smjerovi where sifra=1;   ----tako se ne radi....radit cemo da spo
 
 --- kuharica prema materialima
 
+---želim spojiti naziv grupe i naziv smjera u jednu relaciju
+-- select -- uvjek ostavim prvo praznu ( jer ču na kraju rec sta želim selektirat)
+-- v biveru si pogledaj relaciju između tablica
+-- select -- uvjek ostavim prazan prostor iza selekta ( jer ču na kraju rec sta želim selektirat)
+---TO JE ZADNJE STO NAPISEM --- a.naziv as smjer, b.naziv as grupa
+
+-- onda idem from---i želim -- (ime tablice) SMJEROVI budu A (inner join - relacija, želiš informacije koje su u presjeku kruga A i kruga B) tablica GRUPE budu B
+-- from smjerovi a inner join grupe b
+-- on --- povezuje kolone u tablicama
+-- a.sifra je kolona u tablici smjerovi, b.smjer je kolona u tablici grupe
+-- a = b
+-- on a.sifra = b.smjer
+
+--select a.brojslobodnihmjesta as kolkojihima, b.polaznik as onitamo
+--from grupe a inner join clanovi b
+--on a.sifra = b.grupa
+
 select a.naziv as smjer, b.naziv as grupa
 from smjerovi a inner join grupe b
 on a.sifra = b.smjer
+
+select a. --( smo preimenovali ) 
+select a.naziv as smjer, b.naziv as grupa
+from smjerovi a inner join grupe b
+on  --- po kojem ključu ide veza 
+on a.sifra ---smjerovima sam dao zamjensko ime A
+on a.sifra = b.smjer
+
+--- želimo izvuc sve smjerove 
+--- sad je odabro sve smjerove i one koje nemaju grupe kao sto je žćčđš
+select a.naziv as smjer, b.naziv as grupa
+from smjerovi a left join grupe b
+on a.sifra = b.smjer
+
+
+---želimo sad pokazat ime i prezimena polaznika u grupama
+select a.naziv as smjer, b.naziv as grupe, d.ime, d.prezime 
+-- 1veza
+from smjerovi a inner join grupe b on a.sifra = b.smjer
+-- 2veza
+inner join clanovi c on b.sifra = c.grupa
+-- 3veza
+inner join polaznici d on d.sifra = c.polaznik
+
+--- želim da se pokazuje specifična informaciju
+-- pokaži sva imena koja počnu sa P
+where d.ime like 'P%';
+
+---oreder by znači želim po koloni uredit/posložit tablicu
+select a.naziv as smjer, b.naziv as grupe, d.ime, d.prezime 
+from smjerovi a inner join grupe b on a.sifra = b.smjer
+inner join clanovi c on b.sifra = c.grupa
+inner join polaznici d on d.sifra = c.polaznik
+where d.ime like 'P%'
+order by 4 desc;
+
+--- kolko ima clanova po grupi---znači moramo spojiti grupe i članove!
+select a.naziv count(b.clanovi) as polaznika
+from grupa a inner join clanovi b
+on a.sifra = b.grupa
+grup by a.naziv
+
+
+select a.naziv, count(b.polaznik) as polaznici
+--- from (grupe a) =to pomeni da si dala tabeli grupe ime a) inner join (clanovi b = in tabeli clanovi ime b)
+on a.sifra = b.grupa  --- ON JE VEDNO DEFINIRANA RELACIJA veza MED TABELEMA
+group by a.naziv
+
+---primjer bez a,b,c 
+select grupe.naziv, count(clanovi.polaznik)
+from grupe inner join clanovi
+on grupe.sifra = clanovi.grupa
+group by grupe.naziv
+
+
+
+
 
 select a.naziv as smjer, b.naziv as grupa
 from smjerovi a left join grupe b
