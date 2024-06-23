d4d8e52c30f000de65f085d504bc74bb11e2ee9a
Kristina Andric <fashionbykriss@gmail.com>
2024-06-23 14:21:30 +0200

    Update Program.cs

--- CSHARP/UcenjeCS/UcenjeCS/Program.cs
+++ CSHARP/UcenjeCS/UcenjeCS/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Edunova!");
+Console.WriteLine("Ti si najljepsa cura na svijetu");
+
+// Ovo je komentar jedna linija
+
+/*
+
+Komentar kroz vise linija
+
+*/
